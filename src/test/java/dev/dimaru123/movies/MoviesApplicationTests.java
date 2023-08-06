de57a4534fc190376ce684462ab0fa4b7afdf938
unknown <dimaru123@gmail.com>
2023-08-06 08:25:31 +0800

    commit

---
+++ src/test/java/dev/dimaru123/movies/MoviesApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.dimaru123.movies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoviesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
